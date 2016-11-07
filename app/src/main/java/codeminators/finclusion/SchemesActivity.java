965320f57fd47c435f3955b8b1f9988ed7a3ca27
Prempal Singh <prempal.42@gmail.com>
2016-11-08 02:18:24 +0530

    create skeleton activities

---
+++ app/src/main/java/codeminators/finclusion/SchemesActivity.java
@@ -0,0 +1,13 @@
+package codeminators.finclusion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SchemesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schemes);
+    }
+}
